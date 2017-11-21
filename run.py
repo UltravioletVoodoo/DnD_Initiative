e5fa4443484b560cb221216ce3cbbd05ee00502a
Jonathan <jonbezeau@gmail.com>
2017-11-20 18:24:30 -0800

    Removed test print statements

--- run.py
+++ run.py
@@ -100,12 +100,6 @@ _#/|##########/\######(   /\   )######/\##########|\#_
                   (vvv(VVV)(VVV)vvv)
     """)
 
-    print(Fore.RED + 'some red text')
-    print(Back.GREEN + 'and with a green background')
-    print(Style.DIM + 'and in dim text')
-    print(Style.RESET_ALL)
-    print('back to normal now')
-
     start()
 
 
